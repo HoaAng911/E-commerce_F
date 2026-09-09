34eec454cb860873ef740a3407fc47ec460d935b
HoaAng911 <mayanh279@gmail.com>
2026-09-09 13:52:47 +0700

    chore: remove unused imports and add module comment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,3 +1,4 @@
+// AppModule - main application module
 import { ArticleModule } from './article/article.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
@@ -8,19 +9,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import databaseConfig from './config/database.config';
 import { ProductsModule } from './product/products.module';
-import { Review } from './review/entity/review.entity';
-import { User } from './user/entity/user.entity';
-import { Product } from './product/entity/product.entity';
-import { Category } from './categories/entity/categories.entity';
 import { CartModule } from './cart/cart.module';
-import { CartItem } from './cart/entity/cart-item.entity';
-import { Cart } from './cart/entity/cart.entity';
-import { seconds, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
 import { OrderModule } from './order/order.module';
-import { Order } from './order/entity/order.entity';
-import { OrderItem } from './order/entity/order-item.entity';
-import { Article } from './article/entity/article.entity';
+
 import { ReviewsModule } from './review/review.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
